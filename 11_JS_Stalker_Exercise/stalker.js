716784104bf89a558cd67067e5735576482e35cd
sky3691841 <sky3691841@gmail.com>
2018-06-12 18:15:36 +0800

    update

--- 11_JS_Stalker_Exercise/stalker.js
+++
@@ -1,6 +0,0 @@
-var fName = prompt("What's your first name?");
-var lName = prompt("What's your last name?");
-var age = prompt("How old are you?");
-
-console.log("Your full name is " + fName + " " + lName);
-console.log("You are " + age + " years old");
